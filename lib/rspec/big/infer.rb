2ac5cc1081acfa2432e21c7e858db83d54d5afcf
Patryk Ptasiński <patryk@ipepe.pl>
2025-05-16 02:43:33 +0200

    Initial commit

---
+++ lib/rspec/big/infer.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "infer/version"
+
+module Rspec
+  module Big
+    module Infer
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ spec/rspec/big/infer_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Rspec::Big::Infer do
+  it "has a version number" do
+    expect(Rspec::Big::Infer::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
