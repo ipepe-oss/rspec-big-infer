32264c6c009a6d161aa9450abe838044084403cc
Patryk Ptasiński <patryk@ipepe.pl>
2025-05-16 03:21:08 +0200

    Updated RSpec tests to utilize spec_helper for better configuration.

--- spec/rspec/big/infer_spec.rb
+++ spec/rspec/big/infer_spec.rb
@@ -1,11 +1,9 @@
 # frozen_string_literal: true
 
+require "spec_helper"
+
 RSpec.describe Rspec::Big::Infer do
   it "has a version number" do
     expect(Rspec::Big::Infer::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
   end
 end
